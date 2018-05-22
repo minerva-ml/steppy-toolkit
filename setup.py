906de75722fef50e492e629108589df9e4ac0e01
Grzegorz Łoś <grzegorz.los@codilime.com>
2018-05-22 14:17:06 +0200

    Setup for toolkit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='steppy_toolkit',
+      version='0.0.1',
+      description='Set of tools to make your work with Steps faster and more effective.',
+      url='https://github.com/minerva-ml/steppy-toolkit',
+      author='minerva.ml ',
+      author_email=' ml-team@neptune.ml',
+      license='MIT',
+      packages=['steppy_toolkit'],
+      zip_safe=False
+      )
+
