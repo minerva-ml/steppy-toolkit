78a1a09ae21f29eaf7bfb8a24aa55b0897d6c26a
Kamil A. Kaczmarek <kamil.kaczmarek@neptune.ml>
2018-09-20 15:52:17 +0200

    Dev segmentation (#36)
    * added loaders
    
    * fix pytorch callbacks
    
    * prepare for v0.1.9, dropped not used tests
    
    * Update requirements.txt

---
+++ toolkit/utils.py
@@ -0,0 +1,73 @@
+import os
+import time
+
+import numpy as np
+from PIL import Image
+from pycocotools import mask as cocomask
+from imgaug import augmenters as iaa
+import imgaug as ia
+
+
+def from_pil(*images):
+    images = [np.array(image) for image in images]
+    if len(images) == 1:
+        return images[0]
+    else:
+        return images
+
+
+def to_pil(*images):
+    images = [Image.fromarray((image).astype(np.uint8)) for image in images]
+    if len(images) == 1:
+        return images[0]
+    else:
+        return images
+
+
+def rle_from_binary(prediction):
+    prediction = np.asfortranarray(prediction)
+    return cocomask.encode(prediction)
+
+
+def binary_from_rle(rle):
+    return cocomask.decode(rle)
+
+
+class ImgAug:
+    def __init__(self, augmenters):
+        if not isinstance(augmenters, list):
+            augmenters = [augmenters]
+        self.augmenters = augmenters
+        self.seq_det = None
+
+    def _pre_call_hook(self):
+        seq = iaa.Sequential(self.augmenters)
+        seq = reseed(seq, deterministic=True)
+        self.seq_det = seq
+
+    def transform(self, *images):
+        images = [self.seq_det.augment_image(image) for image in images]
+        if len(images) == 1:
+            return images[0]
+        else:
+            return images
+
+    def __call__(self, *args):
+        self._pre_call_hook()
+        return self.transform(*args)
+
+
+def get_seed():
+    seed = int(time.time()) + int(os.getpid())
+    return seed
+
+
+def reseed(augmenter, deterministic=True):
+    augmenter.random_state = ia.new_random_state(get_seed())
+    if deterministic:
+        augmenter.deterministic = True
+
+    for lists in augmenter.get_children_lists():
+        for aug in lists:
+            aug = reseed(aug, deterministic=True)
+    return augmenter
